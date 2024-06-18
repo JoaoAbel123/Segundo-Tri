34cd8699f727b8add51e0b9bf79b946f84ad0a2a
JoaoABEL123 <107255204+JoaoAbel123@users.noreply.github.com>
2024-06-18 16:07:19 -0300

    aaaaa

--- script.js
+++ script.js
@@ -95,10 +95,10 @@ function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.textContent;
-        botaoAlternativas.addEventListener("click", function()){    
+        botaoAlternativas.addEventListener("click", function(){    
             atual++;
             mostraPergunta();
-        }
+        })
         caixaAlternativas.appendChild(botaoAlternativas); 
     }
 }
