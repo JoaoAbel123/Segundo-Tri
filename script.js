829f12959d4e8b3a2c5e3925fd36c2ead3a6e6c9
JoaoABEL123 <107255204+JoaoAbel123@users.noreply.github.com>
2024-06-18 16:11:24 -0300

    aaaaaaa

--- script.js
+++ script.js
@@ -87,16 +87,17 @@ function mostraPergunta() {
     mostraAlternativas();
 }
 
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.textContent;
-        botaoAlternativas.addEventListener("click", function(){    
-            atual++;
-            mostraPergunta();
-        })
-        caixaAlternativas.appendChild(botaoAlternativas); 
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+      const botaoAlternativas = document.createElement("button");
+      botaoAlternativas.textContent = alternativa.texto;
+      botaoAlternativas.addEventListener("click", function () {
+        atual++;
+        mostraPergunta();
+      });
+      caixaAlternativas.appendChild(botaoAlternativas);
     }
-}
+  }
+  
 
 mostraPergunta();
